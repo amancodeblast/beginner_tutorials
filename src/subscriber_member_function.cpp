1e8170952056181ff692df8533d4dc25137cf127
Aman Sharma <amankrsharma3@gmail.com>
2022-12-05 22:08:36 -0500

    adding the pub sub doxygenated comments

--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -1,16 +1,13 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * @file subscriber_member_function.cpp
+ * @author Aman Sharma (amankrsharma3@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
 #include <signal.h>
 
@@ -36,7 +33,7 @@ void MinimalSubscriber::topic_callback(
 }
 
 /**
- * @brief returns an error stream message when forced to shutdown using ctrl+c
+ * @brief node to be forced stop
  *
  * @param signum
  */
